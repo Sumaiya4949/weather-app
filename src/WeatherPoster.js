6ff21d2390d706f37a46b94b9fc70f058a3f5697
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-01 21:35:01 +0200

    Use ant image

--- src/WeatherPoster.js
+++ src/WeatherPoster.js
@@ -1,6 +1,8 @@
+import { Image } from "antd";
+
 const WeatherPoster = (props) => {
   const { imgUrl } = props;
-  return <img src={imgUrl} />;
+  return <Image width={200} height={200} src={imgUrl} />;
 };
 
 export default WeatherPoster;
