b863214784a242e23628d8c6029b4ad4565dacac
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-03 19:58:56 +0200

    Add nice layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "antd/dist/antd.css";
+import { Layout, Menu } from "antd";
 import CurrentWeatherPanel from "./components/CurrentWeatherPanel";
 import "./styles/App.css";
 
+const { Header, Content, Footer } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <CurrentWeatherPanel />
-    </div>
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">My Location</Menu.Item>
+          <Menu.Item key="2">Any Location</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", marginTop: 64 }}
+      >
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 380 }}
+        >
+          <CurrentWeatherPanel />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Weather App ©2022 Created by Sumaiya Sultana
+      </Footer>
+    </Layout>
   );
 }
 
