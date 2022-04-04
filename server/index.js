0d4321c66c5356f58214aa22110a1259c1d2cdb3
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-04 18:15:29 +0200

    Fetch weather data in the server from rapid api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 5000;
 
@@ -6,28 +7,52 @@ app.get("/", (req, res) => {
   res.send("<html><body><em>Hello World!</em></body></html>");
 });
 
-app.get("/weather-details-by-location", (req, res) => {
-  const { lat, lon, rapiKey } = req.params;
+app.get("/weather-details-by-location", async (req, res) => {
+  const { lat, lon, rapiKey } = req.query;
+
+  const options = {
+    method: "GET",
+    url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
+    params: { lon, lat },
+    headers: {
+      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+      "X-RapidAPI-Key": rapiKey,
+    },
+  };
+
+  const response = await axios.request(options);
+  const weatherReport = response.data.data[0];
+
+  const {
+    weather,
+    temp,
+    rh,
+    pres,
+    country_code,
+    state_code,
+    city_name,
+    ob_time,
+  } = weatherReport;
 
   res.send({
     weather: {
-      title: "Very cloudy",
-      temperature: "50°c",
-      relativeHumidity: "100",
-      airPressure: "1000atm",
-      imgUrl: "icons/s04d.png",
+      title: weather.description,
+      temperature: `${temp}°c`,
+      imgUrl: `icons/${weather.icon}.png`,
+      relativeHumidity: rh,
+      airPressure: `${pres}atm`,
     },
     locationDetails: {
-      city: "Chemnitz",
-      state: "Saxony",
-      country: "Germany",
+      city: city_name,
+      state: state_code,
+      country: country_code,
     },
-    datetime: "12-12-12",
+    datetime: new Date(`${ob_time} UTC`).toLocaleString(),
   });
 });
 
 app.get("/weather-forecast-by-location", (req, res) => {
-  const { lat, lon, rapidKey } = req.params;
+  const { lat, lon, rapidKey } = req.query;
 
   res.send([
     {
