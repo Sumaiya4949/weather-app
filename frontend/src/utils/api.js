f3566e5bb6378aa9017e50e05efc559addb2df74
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-04 18:34:31 +0200

    Implement both api in server

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,21 +1,5 @@
 import axios from "axios";
 
-function f(coord) {
-  return parseInt(coord * 1000, 10) / 1000;
-}
-
-function saveLocally(api, lat, lon, data) {
-  sessionStorage.setItem(`${api}-${f(lat)}-${f(lon)}`, JSON.stringify(data));
-}
-
-function hasLocally(api, lat, lon) {
-  return !!sessionStorage.getItem(`${api}-${f(lat)}-${f(lon)}`);
-}
-
-function getLocally(api, lat, lon) {
-  return JSON.parse(sessionStorage.getItem(`${api}-${f(lat)}-${f(lon)}`));
-}
-
 export const fetchCurrentWeather = async (lat, lon) => {
   const options = {
     method: "GET",
@@ -27,51 +11,6 @@ export const fetchCurrentWeather = async (lat, lon) => {
   return data;
 };
 
-export const fetchCurrentWeather_old = async (lat, lon) => {
-  if (hasLocally("current-weather", lat, lon)) {
-    return getLocally("current-weather", lat, lon);
-  }
-
-  const options = {
-    method: "GET",
-    url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
-    params: { lon, lat },
-    headers: {
-      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    },
-  };
-
-  const response = await axios.request(options);
-  const weatherReport = response.data.data[0];
-  const {
-    weather,
-    temp,
-    rh,
-    pres,
-    country_code,
-    state_code,
-    city_name,
-    ob_time,
-  } = weatherReport;
-
-  const formattedReport = {
-    title: weather.description,
-    temperature: temp,
-    imgUrl: `icons/${weather.icon}.png`,
-    humidity: rh,
-    airPressure: pres,
-    country: country_code,
-    state: state_code,
-    city: city_name,
-    date: new Date(`${ob_time} UTC`).toLocaleString(),
-  };
-
-  saveLocally("current-weather", lat, lon, formattedReport);
-
-  return formattedReport;
-};
-
 export const fetchWeatherForecast = async (lat, lon) => {
   const options = {
     method: "GET",
@@ -81,36 +20,4 @@ export const fetchWeatherForecast = async (lat, lon) => {
 
   const { data } = await axios.request(options);
   return data;
-};
-
-export const fetchWeatherForecast_old = async (lat, lon) => {
-  if (hasLocally("forecast", lat, lon)) {
-    return getLocally("forecast", lat, lon);
-  }
-
-  const options = {
-    method: "GET",
-    url: "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly",
-    params: { lat, lon },
-    headers: {
-      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    },
-  };
-
-  const response = await axios.request(options);
-  const forecast = response.data.data;
-  const processedForecast = forecast.map((item) => {
-    const [date, time] = item.timestamp_local.split("T");
-
-    return {
-      imgUrl: `icons/${item.weather.icon}.png`,
-      date,
-      time,
-    };
-  });
-
-  saveLocally("forecast", lat, lon, processedForecast);
-
-  return processedForecast;
 };
--- server/index.js
+++ server/index.js
@@ -3,12 +3,39 @@ const axios = require("axios");
 const app = express();
 const port = 5000;
 
+const responseMap = {};
+
+function f(coord) {
+  return parseInt(coord * 1000, 10) / 1000;
+}
+
+function saveLocally(api, lat, lon, data) {
+  responseMap[`${api}-${f(lat)}-${f(lon)}`] = data;
+}
+
+function hasLocally(api, lat, lon) {
+  return !!responseMap[`${api}-${f(lat)}-${f(lon)}`];
+}
+
+function getLocally(api, lat, lon) {
+  return responseMap[`${api}-${f(lat)}-${f(lon)}`];
+}
+
 app.get("/", (req, res) => {
   res.send("<html><body><em>Hello World!</em></body></html>");
 });
 
 app.get("/weather-details-by-location", async (req, res) => {
   const { lat, lon, rapiKey } = req.query;
+
+  if (hasLocally("current-weather", lat, lon)) {
+    console.log("current-weather returning from cache");
+    res.send(getLocally("current-weather", lat, lon));
+    res.end();
+    return;
+  }
+
+  console.log("Requesting current weather from rapid api");
 
   const options = {
     method: "GET",
@@ -34,7 +61,7 @@ app.get("/weather-details-by-location", async (req, res) => {
     ob_time,
   } = weatherReport;
 
-  res.send({
+  const formattedReport = {
     weather: {
       title: weather.description,
       temperature: `${temp}°c`,
@@ -48,29 +75,49 @@ app.get("/weather-details-by-location", async (req, res) => {
       country: country_code,
     },
     datetime: new Date(`${ob_time} UTC`).toLocaleString(),
-  });
+  };
+
+  res.send(formattedReport);
+
+  saveLocally("current-weather", lat, lon, formattedReport);
 });
 
-app.get("/weather-forecast-by-location", (req, res) => {
-  const { lat, lon, rapidKey } = req.query;
+app.get("/weather-forecast-by-location", async (req, res) => {
+  const { lat, lon, rapiKey } = req.query;
 
-  res.send([
-    {
-      imgUrl: "icons/s04d.png",
-      date: "20-20-2022",
-      time: "13:15",
+  if (hasLocally("forecast", lat, lon)) {
+    console.log("forecast returning from cache");
+    res.send(getLocally("forecast", lat, lon));
+    res.end();
+    return;
+  }
+
+  console.log("Requesting forecast from rapid api");
+
+  const options = {
+    method: "GET",
+    url: "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly",
+    params: { lat, lon },
+    headers: {
+      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+      "X-RapidAPI-Key": rapiKey,
     },
-    {
-      imgUrl: "icons/s04d.png",
-      date: "20-20-2022",
-      time: "13:15",
-    },
-    {
-      imgUrl: "icons/s04d.png",
-      date: "20-20-2022",
-      time: "13:15",
-    },
-  ]);
+  };
+
+  const response = await axios.request(options);
+  const forecast = response.data.data;
+  const processedForecast = forecast.map((item) => {
+    const [date, time] = item.timestamp_local.split("T");
+
+    return {
+      imgUrl: `icons/${item.weather.icon}.png`,
+      date,
+      time,
+    };
+  });
+
+  res.send(processedForecast);
+  saveLocally("forecast", lat, lon, processedForecast);
 });
 
 app.listen(port, () => {
