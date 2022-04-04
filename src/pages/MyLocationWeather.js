7637f17937ff75475ee4d8d777ddcb4348a6ba78
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-04 10:13:49 +0200

    Visualize current location's weather successfully

--- src/pages/MyLocationWeather.js
+++ src/pages/MyLocationWeather.js
@@ -1,5 +1,43 @@
-const MyLocationWeather = (props) => {
-  return "My location weather page";
+import { useEffect } from "react";
+import WeatherReport from "../components/WeatherReport";
+import { Result, Row, Col, Card } from "antd";
+import FutureWeatherForecast from "../components/FutureWeatherForecast";
+import { AntCloudOutlined } from "@ant-design/icons";
+import useWeatherData from "../hooks/useWeatherData";
+
+const MyLocationWeather = () => {
+  const { currentWeatherReport, weatherForecastList, updateLocation } =
+    useWeatherData();
+
+  function handleGeoLocation(location) {
+    const { latitude, longitude } = location.coords;
+    updateLocation(latitude, longitude);
+  }
+
+  useEffect(function () {
+    navigator.geolocation.getCurrentPosition(handleGeoLocation);
+  }, []);
+
+  return (
+    <Row>
+      <Col span={16}>
+        {currentWeatherReport === null ? (
+          <Card style={{ margin: "20px" }}>
+            <Result
+              title="No weather report available for the specified location."
+              icon={<AntCloudOutlined />}
+            />
+          </Card>
+        ) : (
+          <WeatherReport report={currentWeatherReport} />
+        )}
+      </Col>
+
+      <Col span={8}>
+        <FutureWeatherForecast forecastList={weatherForecastList} />
+      </Col>
+    </Row>
+  );
 };
 
 export default MyLocationWeather;
