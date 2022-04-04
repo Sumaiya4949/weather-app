8ec0381ba0cae026c1eb559bb14e4b8a10ce4e2f
sumaiya4949 <sumaiya15-4949@diu.edu.bd>
2022-04-04 10:33:32 +0200

    Add app logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "antd/dist/antd.css";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Typography } from "antd";
 import { useState } from "react";
 import CurrentWeatherPanel from "./components/CurrentWeatherPanel";
 import "./styles/App.css";
 import MyLocationWeather from "./pages/MyLocationWeather";
 
 const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 function App() {
   const [menuKey, setMenuKey] = useState("2");
@@ -17,7 +18,10 @@ function App() {
   return (
     <Layout>
       <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-        <div className="logo" />
+        <Title className="logo" level={2}>
+          Weather App
+        </Title>
+
         <Menu
           onClick={handleMenuChange}
           theme="dark"
@@ -28,6 +32,7 @@ function App() {
           <Menu.Item key="2">Any Location</Menu.Item>
         </Menu>
       </Header>
+
       <Content
         className="site-layout"
         style={{ padding: "0 50px", marginTop: 64 }}
@@ -39,6 +44,7 @@ function App() {
           {menuKey === "1" ? <MyLocationWeather /> : <CurrentWeatherPanel />}
         </div>
       </Content>
+
       <Footer style={{ textAlign: "center" }}>
         Weather App ©2022 Created by Sumaiya Sultana
       </Footer>
